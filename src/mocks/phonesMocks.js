9bdb8cc4b94986d86cb6afa75fc7af631cd5547f
Róman Demchuk <roman.demchuk@gmail.com>
2023-07-12 13:05:32 +0200

    Add mocks for testing. Add sonar github workflow

---
+++ src/mocks/phonesMocks.js
@@ -0,0 +1,18 @@
+export const mockPhones = [
+  {
+    id: "skjDqIJgq-OGRaUNXXCCu",
+    brand: "Acer",
+    model: "DX900",
+    price: "90",
+    imgUrl:
+      "https://itx-frontend-test.onrender.com/images/skjDqIJgq-OGRaUNXXCCu.jpg",
+  },
+  {
+    id: "AasKFs5EGbyAEIKkcHQcF",
+    brand: "alcatel",
+    model: "Flash (2017)",
+    price: "",
+    imgUrl:
+      "https://itx-frontend-test.onrender.com/images/AasKFs5EGbyAEIKkcHQcF.jpg",
+  },
+];
